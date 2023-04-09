a005c4da6ed44b2b75a4e725af6bf32f7ae47356
yash2301 <sanatharayash@gmail.com>
2023-04-10 01:22:50 +0530

    update Api for timelines

--- routes/api.php
+++ routes/api.php
@@ -73,7 +73,11 @@ Route::group(['middleware' => ['auth:sanctum']], function(){
      //Tasks Post
     Route::post('/tasks/update_task_form', [TaskController::class, 'update_task_form']);
     
-    Route::apiResource('timelines', TimelineController::class);
+
+    // create timelines
+    Route::get('/timelines', [TaskController::class, 'store']);
+    // list timelines
+    Route::post('/timelines', [TaskController::class, 'index']);
 
 });
 
