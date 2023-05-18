ecb40e1db4c213e5ed982949b31bd8dccf6eb6c0
Akshay <akshay@inquisithealth.com>
2023-05-17 21:56:05 -0400

    task list update

--- app/Http/Models/Task.php
+++ app/Http/Models/Task.php
@@ -18,11 +18,10 @@ class Task extends Model {
 
     public static function get_task_list(Int $org_id, Int $user_id){
 
-        $list = DB::select("SELECT CONCAT(u.first_name, ' ' ,u.last_name) AS last_updated_by_name, 
-        cv.part_id, cv.station_id, cv.vnum_id, cv.form_id, 
-        IF(cv.compliance_ind = 1, 'Yes', 'No') AS compliance_ind, 
-        cd.form_data, fs.form_json, cd.last_updated_id, 
-        s.name AS station_name, p.name AS part_name
+        $list = DB::select("SELECT s.name AS station_name, p.name AS part_name, cv.vnum_id, cv.form_id, IF(cv.compliance_ind = 1, 'Yes', 'No') AS compliance_ind, CONCAT(u.first_name, ' ' ,u.last_name) AS last_updated_by_name, 
+        cv.part_id, cv.station_id, cd.last_updated_id, 
+        cd.form_data, fs.form_json
+        
         FROM checklist_vnum_record cv 
         LEFT JOIN checklist_data cd ON cd.checklist_vnum_record_id = cv.id
         LEFT JOIN stations s ON s.id = cv.station_id
